01db016da8e539b1f9f585202a41d1cad0c542e3
ARRABITO Luisa <arrabito@in2p3.fr>
2019-12-17 14:50:34 +0100

    testing

--- CTAPilot/CTAPilotCommands.py
+++ CTAPilot/CTAPilotCommands.py
@@ -2,16 +2,41 @@ __RCSID__ = "$Id$"
 
 import os
 
+from pilotCommands import GetPilotVersion, \
+    InstallDIRAC, \
+    ConfigureBasics, \
+    ConfigureCPURequirements, \
+    ConfigureSite, \
+    ConfigureArchitecture
 
 from pilotTools import CommandBase
 
+class CTACommandBase(CommandBase):
+  """ Simple extension, just for LHCb parameters
+  """
 
-class CTASetEnv(CommandBase):
-  """ Set custom CTA environment
+  def __init__(self, pilotParams):
+    """ c'tor
+    """
+    super(CTACommandBase, self).__init__(pilotParams)
+    pilotParams.pilotCFGFile = 'pilot.json'
+    pilotParams.localConfigFile = 'pilot.cfg'
+    pilotParams.architectureScript = 'dirac-architecture'
+
+
+class CTAGetPilotVersion(CTACommandBase, GetPilotVersion):
+  """ Base is enough (pilotParams.pilotCFGFile = 'LHCb-pilot.json')
+  """
+  pass
+
+class CTAConfigureArchitecture(CTACommandBase, ConfigureArchitecture):
+  """ just sets the CMTCONFIG variable
   """
 
   def execute(self):
-    """ Standard method for pilot commands
+    """ calls the superclass execute and then sets the CMTCONFIG variable with the host binary tag
     """
 
-    self.pp.installEnv['DIRACSYSCONFIG'] = os.path.join(os.environ['DIRAC'],'pilot.cfg')
+    # This sets the DIRAC architecture
+    super(CTAConfigureArchitecture, self).execute()
+
